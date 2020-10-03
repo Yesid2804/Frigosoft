e33565c0e103381742edf4f32709e1f7c9f3e571
yesid <yesher2804@gmail.com>
2020-10-03 00:41:06 -0500

    ok

--- Presentacion/Proveedores/FormProveedores.Designer.cs
+++ Presentacion/Proveedores/FormProveedores.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -44,12 +45,14 @@
             this.txtContacto = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.direccion = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Categoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.contacto = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dtgvProveedores = new System.Windows.Forms.DataGridView();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dtgvProveedores)).BeginInit();
             this.SuspendLayout();
             // 
@@ -308,6 +311,7 @@
             this.Load += new System.EventHandler(this.FormProveedores_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dtgvProveedores)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -335,6 +339,10 @@
         private System.Windows.Forms.CheckBox checkBoxRes;
         private System.Windows.Forms.TextBox txtDireccion;
         private System.Windows.Forms.TextBox txtContacto;
+<<<<<<< Updated upstream
+=======
+        private System.Windows.Forms.BindingSource bindingSource1;
+>>>>>>> Stashed changes
         private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
         private System.Windows.Forms.DataGridViewTextBoxColumn direccion;
         private System.Windows.Forms.DataGridViewTextBoxColumn Categoria;
--- Presentacion/Reportes/FormReporteCompras.Designer.cs
+++ Presentacion/Reportes/FormReporteCompras.Designer.cs
@@ -30,7 +30,11 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+<<<<<<< Updated upstream
             this.comboxFiltroFechas = new System.Windows.Forms.ComboBox();
+=======
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+>>>>>>> Stashed changes
             this.label2 = new System.Windows.Forms.Label();
             this.columnFecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.columnProveedor = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -56,7 +60,11 @@
             // groupBox1
             // 
             this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+<<<<<<< Updated upstream
             this.groupBox1.Controls.Add(this.comboxFiltroFechas);
+=======
+            this.groupBox1.Controls.Add(this.comboBox1);
+>>>>>>> Stashed changes
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Location = new System.Drawing.Point(63, 70);
             this.groupBox1.Name = "groupBox1";
@@ -64,6 +72,7 @@
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             // 
+<<<<<<< Updated upstream
             // comboxFiltroFechas
             // 
             this.comboxFiltroFechas.FormattingEnabled = true;
@@ -76,6 +85,20 @@
             this.comboxFiltroFechas.Size = new System.Drawing.Size(130, 23);
             this.comboxFiltroFechas.TabIndex = 1;
             this.comboxFiltroFechas.Text = "Semana";
+=======
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Dia",
+            "Semana",
+            "Mes",
+            "Año"});
+            this.comboBox1.Location = new System.Drawing.Point(88, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(130, 23);
+            this.comboBox1.TabIndex = 1;
+>>>>>>> Stashed changes
             // 
             // label2
             // 
@@ -127,6 +150,7 @@
             this.columnsFecha.Resizable = System.Windows.Forms.DataGridViewTriState.True;
             this.columnsFecha.Width = 120;
             // 
+<<<<<<< Updated upstream
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -142,11 +166,31 @@
             this.dataGridView1.TabIndex = 2;
             this.dataGridView1.Text = "dataGridView1";
             // 
+=======
+>>>>>>> Stashed changes
             // FormReporteCompras
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1050, 600);
+<<<<<<< Updated upstream
+=======
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.columnProveedor,
+            this.columnProducto,
+            this.columnCantidad,
+            this.columnTotalCompra,
+            this.columnsFecha});
+            this.dataGridView1.Location = new System.Drawing.Point(63, 173);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(643, 312);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.Text = "dataGridView1";
+>>>>>>> Stashed changes
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
@@ -165,7 +209,11 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox1;
+<<<<<<< Updated upstream
         private System.Windows.Forms.ComboBox comboxFiltroFechas;
+=======
+        private System.Windows.Forms.ComboBox comboBox1;
+>>>>>>> Stashed changes
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.DataGridViewTextBoxColumn columnFecha;
         private System.Windows.Forms.DataGridViewTextBoxColumn columnProveedor;
